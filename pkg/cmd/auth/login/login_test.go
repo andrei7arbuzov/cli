9dafa530195fc1c125bf71576dbeec2f8b02a60a
vilmibm <vilmibm@neongrid.space>
2020-07-28 16:41:37 -0500

    WIP testsss

--- pkg/cmd/auth/login/login_test.go
+++ pkg/cmd/auth/login/login_test.go
@@ -2,9 +2,13 @@ package login
 
 import (
 	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
+	"github.com/cli/cli/internal/config"
 	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/pkg/httpmock"
 	"github.com/cli/cli/pkg/iostreams"
 	"github.com/google/shlex"
 	"github.com/stretchr/testify/assert"
@@ -118,22 +122,46 @@ func Test_NewCmdLogin(t *testing.T) {
 	}
 }
 
-/*
 func Test_loginRun(t *testing.T) {
 	tests := []struct {
 		name       string
 		opts       *LoginOptions
 		tty        bool
+		httpStubs  func(*httpmock.Registry)
 		wantStdout string
 		wantStderr string
-		wantErr    bool
+		wantHosts  string
+		wantsErr   bool
 	}{
 		{
-			name:    "non-tty error",
-			tty:     false,
-			opts:    &LoginOptions{},
-			wantErr: true,
+			name:     "non-tty no arguments",
+			opts:     &LoginOptions{},
+			wantsErr: true,
 		},
+		{
+			name: "non-tty with token",
+			opts: &LoginOptions{
+				Token: "abc123",
+			},
+			wantHosts: "LOL TODO",
+			httpStubs: func(reg *httpmock.Registry) {
+				reg.Register(
+					httpmock.REST("GET", "user"),
+					func(req *http.Request) (*http.Response, error) {
+						return &http.Response{
+							StatusCode: 200,
+							Request:    req,
+							Header: map[string][]string{
+								"X-Oauth-Scopes": {"repo,read:org"},
+							},
+							Body: ioutil.NopCloser(bytes.NewBufferString("")),
+						}, nil
+					},
+				)
+			},
+		},
+		// TODO non-tty with token + hostname
+		// TODO non-tty with hostname
 	}
 
 	for _, tt := range tests {
@@ -142,18 +170,40 @@ func Test_loginRun(t *testing.T) {
 		io.SetStdinTTY(tt.tty)
 		io.SetStdoutTTY(tt.tty)
 
+		tt.opts.Config = func() (config.Config, error) {
+			return config.NewBlankConfig(), nil
+		}
+
+		reg := &httpmock.Registry{}
+		tt.opts.HttpClient = func() (*http.Client, error) {
+			return &http.Client{Transport: reg}, nil
+		}
+
+		if tt.httpStubs != nil {
+			tt.httpStubs(reg)
+		}
+
 		tt.opts.IO = io
 		t.Run(tt.name, func(t *testing.T) {
-			if err := loginRun(tt.opts); (err != nil) != tt.wantErr {
-				t.Errorf("loginRun() error = %v, wantErr %v", err, tt.wantErr)
+			mainBuf := bytes.Buffer{}
+			// TODO DEBUG WHY hostsBuf is EMPTY
+			hostsBuf := bytes.Buffer{}
+			defer config.StubWriteConfig(&mainBuf, &hostsBuf)()
+
+			if err := loginRun(tt.opts); (err != nil) != tt.wantsErr {
+				t.Errorf("loginRun() error = %v, wantErr %v", err, tt.wantsErr)
 			}
+
+			defer reg.Verify(t)
 
 			assert.Equal(t, tt.wantStdout, stdout.String())
 			assert.Equal(t, tt.wantStderr, stderr.String())
+			assert.Equal(t, tt.wantHosts, hostsBuf.String())
 		})
 	}
 }
 
+/*
 func Test_loginRun_ConfiguresProtocol(t *testing.T) {
 	io, _, _, _ := iostreams.Test()
 
